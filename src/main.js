615bc93e5c35509b6d8bccbea5f5f33790b80f4a
Vladimir <vlad89main@gmail.com>
2026-01-19 12:38:29 +0300

    feat: step5 sorting redone

--- src/main.js
+++ src/main.js
@@ -42,14 +42,10 @@ async function render(action) {
     let query = {}; // копируем для последующего изменения
 
     // @todo: использование
-    // result = applySorting(result, state, action);
-
-    // result = applySearching(result, state, action);
 
     query = applySearching(query, state, action);
-
     query = applyFiltering(query, state, action);
-
+    query = applySorting(query, state, action);
     query = applyPagination(query, state, action);
 
     const { total, items } = await API.getRecords(query);
